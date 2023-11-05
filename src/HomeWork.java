37ffbfbf17a8878b6bbdafa0386d5070a633c611
SergeyKupchinskiy <149712939+SergeyKupchinskiy@users.noreply.github.com>
2023-11-05 20:49:52 +0300

    Update HomeWork.java

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HomeWork {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите имя пользователя");
+        System.out.println("Enter User Name");
         String username = sc.nextLine();
         System.out.println("Hello," + username + "!");
     }
